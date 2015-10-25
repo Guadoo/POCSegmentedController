5132e2f2974f18e4dac677b097badb9e329a136b
Guadoo <rock_jk@msn.com>
2015-10-25 20:21:16 +0800

    Initial Commit

---
+++ POCSegmentedController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POCSegmentedController
+//
+//  Created by Liu Jingkai on 15/10/25.
+//  Copyright (c) 2015年 Liu Jingkai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
