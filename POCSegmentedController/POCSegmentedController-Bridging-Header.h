216128a7d1b869ec47ec58e891956cb93e73ce29
Guadoo <rock_jk@msn.com>
2015-10-31 09:29:26 +0800

    Initial segment and customize it

---
+++ POCSegmentedController/POCSegmentedController-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//Import "DZNSegmentedControl.h"
+
+//#import "ASDayPicker.h"
+//#import <pop/POP.h>
+
+#import <DZNSegmentedControl/DZNSegmentedControl.h>
---
+++ POCSegmentedController/TableViewController.swift
@@ -0,0 +1,158 @@
+//
+//  TableViewController.swift
+//
+//
+//  Created by Liu Jingkai on 15/10/25.
+//  Copyright (c) 2015年 Liu Jingkai. All rights reserved.
+//
+
+import UIKit
+
+let DEBUG_APPERANCE = 0
+let DEBUG_IMAGE     = 0
+
+let kBakgroundColor = UIColor(red: 0/255.0, green: 87/255.0, blue: 173/255.0, alpha: 1.0)
+let kTinkColor      = UIColor(red: 20/255.0, green: 200/255.0, blue: 255/255.0, alpha: 1.0)
+let kHairlineColor  = UIColor(red: 0/255.0, green: 36/255.0, blue: 100/255.0, alpha: 1.0)
+
+class TableViewController: UITableViewController, DZNSegmentedControlDelegate {
+    
+    var control: DZNSegmentedControl?
+    var durationItems: NSArray = []
+       
+    override func loadView() {
+        super.loadView()
+        
+        // To do - Need to change to duration date
+        //title = NSStringFromClass(DZNSegmentedControl)
+        
+        var today = NSDate()
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        // Date 转 String
+        title = dateFormatter.stringFromDate(today)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        durationItems = ["Day".uppercaseString, "Week".uppercaseString, "Year".uppercaseString, "Total".uppercaseString]
+        
+        tableView.tableHeaderView = self.fControl()
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func fControl() -> DZNSegmentedControl{
+        
+        if control == nil {
+            control = DZNSegmentedControl(items: durationItems as [AnyObject])
+            control?.delegate = self
+            control?.selectedSegmentIndex = 0
+            control?.bouncySelectionIndicator = false
+            
+            //control?.addTarget(self, action: "selectedSegment:", forControlEvents: UIControlEvents.ValueChanged)
+            
+        }
+        
+        return control!
+        
+    }
+    
+    // MARK: - UIBarPositioningDelegate Methods
+    
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return UIBarPosition.Bottom
+    }
+    
+    
+//    - (UIBarPosition)positionForBar:(id <UIBarPositioning>)view
+//    {
+//    return UIBarPositionBottom;
+//    }
+    
+
+    
+    
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
